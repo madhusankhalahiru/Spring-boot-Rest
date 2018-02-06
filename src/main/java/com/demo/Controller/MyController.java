865c306b23f357928806cc24c9ffdfdf610c9be9
Lahiru madhusankha gamage <madhusankhalahiru@gmail.com>
2018-02-06 19:14:27 +0530

    change mycontroller
    adding rest methods

--- src/main/java/com/demo/Controller/MyController.java
+++ src/main/java/com/demo/Controller/MyController.java
@@ -5,20 +5,43 @@ import com.demo.model.Note;
 import com.demo.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class MyController {
 
-
+    @Autowired
+    NoteRepository noteRepository;
 
     @GetMapping(path = "/fun")
     public @ResponseBody
     String hello() {
         return "Hello my controller";
+    }
+
+    @GetMapping("/notes")
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
+    }
+
+    // Create a new Note
+    @PostMapping("/notes")
+    public Note createNote(@Valid @RequestBody Note note) {
+        return noteRepository.save(note);
+    }
+
+    @GetMapping("/notes/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable(value = "id") Long noteId) {
+        Note note = noteRepository.findOne(noteId);
+        if(note == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(note);
     }
 
 }
